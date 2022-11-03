3094e2f7f72efd9ee3e6927e703970790b997699
unknown <ranis.krd@gmail.com>
2022-11-03 15:30:44 +0300

    first commit

--- comebacker/script.js
+++ comebacker/script.js
@@ -6,20 +6,44 @@ $(function() {
 
 	var comebacker = true;
 	
-	$(window).mouseout(function(e){
-		if(e.pageY - $(window).scrollTop() < 1 && comebacker == true){
+	// $(window).mouseout(function(e){
+	// 	// На ===MouseOut===
+	// 	if(e.pageY - $(window).scrollTop() < 1 && comebacker == true){
+	// 		$('#comebacker').fadeIn(300); 
+	// 		$('body').addClass('comebackerhidden');
+	// 		comebacker = false;
+	// 	}
+	// });
+
+	$(document).keyup(function(e) {
+		// На нажатие ===Backspace===
+		if(e.key === 'Backspace' && comebacker){
 			$('#comebacker').fadeIn(300); 
 			$('body').addClass('comebackerhidden');
 			comebacker = false;
 		}
 	});
 
+
+	$(window).on("beforeunload", function() {
+			$('#comebacker').fadeIn(300); // 300 скорость появления | appear speed 
+			$('body').addClass('comebackerhidden');
+			comebacker = false;
+		return "Вы уверены, что хотите покинуть страницу?";
+
+	});
+
+	
+
 	try {
 		setTimeout(
 			function show_comebacker() {
-				$('#comebacker').fadeIn(300); // 300 скорость появления | appear speed 
-				$('body').addClass('comebackerhidden');
-			}, 3000 //Время появления в милисекундах | Appear time in milliseconds
+				if(comebacker) {
+					$('#comebacker').fadeIn(300); // 300 скорость появления | appear speed 
+					$('body').addClass('comebackerhidden');
+					comebacker = false;
+				}
+			}, 10000 //Время появления в милисекундах | Appear time in milliseconds
 		)
 	}
 	catch (e) {}
